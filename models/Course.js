6eabe0443da3e43cdc7f076afcef36a23af9dbff
Milen961 <theheller178@gmail.com>
2022-10-20 15:12:58 +0300

    Creating courseController and createTemplate

--- models/Course.js
+++ models/Course.js
@@ -3,19 +3,21 @@ const { Schema, model, Types } = require('mongoose');
 const URL_PATTERN = /https?:\/\/./i;
 
 const courseSchema = new Schema({
-    title: { type: String, required: true, minlength: [4, 'Course title must be at least 4 characters long'] },
+    title: { type: String, minlength: [4, 'Course title must be at least 4 characters long'] },
     description: {
-        type: String, required: true, minlength: [20, 'Course description must be at least 20 characters long'],
+        type: String,
+        minlength: [20, 'Course description must be at least 20 characters long'],
         maxlength: [50, 'Course description must be at most 50 characters long']
     },
     imageUrl: {
-        type: String, required: true, validate: {
+        type: String, 
+        validate: {
             validator: (value) => URL_PATTERN.test(value),
             message: 'Invalid URL'
         }
     },
-    duration: { type: String, required: true},
-    createdAt: { type: String, required: true},
+    duration: { type: String, required: [true, 'Duration is required']},
+    createdAt: { type: String, required: true, default: () => (new Date()).toISOString().slice(0, 10 ) },
     users: { type: [Types.ObjectId], ref: 'User', default: [] },
     owner: { type: Types.ObjectId, ref: 'User' }
 
@@ -30,4 +32,4 @@ courseSchema.index({ title: 1 },{
 
 const Course = model('Course', courseSchema);
 
-model.exports = Course
+module.exports = Course
